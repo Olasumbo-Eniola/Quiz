8b77e96f7e0f89f2733a808bc80d3872b992f4b3
Olasumbo-Eniola <olasumboeniola1@gmail.com>
2020-05-05 09:03:22 -0800

    commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 var i = 0;
-var b = 0;
 
 function NextQu(){
   i++;
@@ -10,6 +9,8 @@ function NextQu(){
   document.getElementById('CurrentQuScore').style.display = 'none';
   document.getElementById('Answer1').style.display = "inline";
   document.getElementById('Answer1').style.backgroundColor = "white"
+    document.getElementById('Answer2').style.backgroundColor = 'white';
+    document.getElementById('Answer3').style.backgroundColor = 'white';
   document.getElementById('Answer2').style.display = "inline";
   document.getElementById('Answer3').style.display ="inline";
   document.getElementById("Answer1").innerHTML=Answers[i][0];
@@ -32,15 +33,42 @@ function FinishQu() {
 var j;
 var ScoreTotal = [];
 
+
+var Q1 = 'Which planet is known as the red planet';
+var Q2 = 'What is the closest planet to the sun?';
+var Q3 = 'Where can sun be classified?';
+var Q4 = 'Which galaxy is earth located?';
+var Q5 = ' What is the name of NASA’s most famous space telescope?';
+var Questions = [Q1, Q2, Q3, Q4, Q5];
+
+var A1 = ['Jupiter', 'Saturn', 'Mars']
+var A2 = ['Uranus', 'Saturn', 'Mercury'];
+var A3 = ['Planet', 'No idea', 'Star'];
+var A4 = ['Andromeda Galaxy', 'Whirlpool Galaxy', 'The Milky Way Galaxy'];
+var A5 = ['James Webb telescope', 'Spitzer space telescope', 'Hubble Space telescope'];
+var Answers = [A1, A2, A3, A4, A5];
+
+ 
+var SC1 = [0, 0, 10];
+var SC2 = [0, 0, 10];
+var SC3 = [0, 0, 10];
+var SC4 = [0, 0, 10];
+var SC5 = [0, 0, 10];
+var SC6 = [0, 0, 10];
+var score = [SC1, SC2, SC3, SC4, SC5, SC6];
+function getSum(a, b){
+  return a + b;
+}
+document.getElementById("QuestionQuiz").innerHTML = "Question 1:"+Questions[i];
+document.getElementById("QuestionQuiz").style.display = 'block';
+document.getElementById("Answer1").innerHTML=Answers[i][0];
+document.getElementById("Answer2").innerHTML=Answers[i][1];
+document.getElementById("Answer3").innerHTML=Answers[i][2];
 function Answer1Select(){
-  do{
-    Soln = b + 1;
-    document.getElementById('Answer1').style.backgroundColor = 'greenyellow'
-  }while(Answers[i].values !== Soln[b]);
-  
   if (i < Questions.length - 1){
   j=0;
-  
+  document.getElementById('Answer1').style.backgroundColor = 'red';
+      document.getElementById('Answer3').style.backgroundColor = 'greenyellow';
   document.getElementById('Next').style.display = "inline";
   document.getElementById('Answer1').disabled = true;
   document.getElementById('Answer2').disabled = true;
@@ -51,6 +79,8 @@ function Answer1Select(){
   document.getElementById("Rolling Score").innerHTML="Your total score is"+'<br>'+ScoreTotal.reduce(getSum);
       
   }else{
+      document.getElementById('Answer1').style.backgroundColor = 'red';
+      document.getElementById('Answer3').style.backgroundColor = 'greenyellow';
     document.getElementById("Finish").style.display = "inline";
   document.getElementById("CurrentQuScore").style.display = "block";
   document.getElementById("Answer1").disabled = true;
@@ -65,6 +95,8 @@ function Answer1Select(){
 function Answer2Select(){
   if (i < Questions.length - 1){
     j=1;
+      document.getElementById('Answer2').style.backgroundColor = 'red';
+      document.getElementById('Answer3').style.backgroundColor = 'greenyellow';
     document.getElementById('Next').style.display = "inline";
     document.getElementById('Answer1').disabled = true;
     document.getElementById('Answer2').disabled = true;
@@ -74,6 +106,8 @@ function Answer2Select(){
     ScoreTotal.push(score[i][j])
     document.getElementById("Rolling Score").innerHTML="Your total score is"+'<br>'+ScoreTotal.reduce(getSum);
     }else{
+        document.getElementById('Answer2').style.backgroundColor = 'red';
+      document.getElementById('Answer3').style.backgroundColor = 'greenyellow';
       document.getElementById("Finish").style.display = "inline";
     document.getElementById("CurrentQuScore").style.display = "block";
           document.getElementById("Answer1").disabled = true;
@@ -87,6 +121,7 @@ function Answer2Select(){
 function Answer3Select(){
   if (i < Questions.length - 1){
     j=2;
+      document.getElementById('Answer3').style.backgroundColor = 'greenyellow';
     document.getElementById('Next').style.display = "inline";
     document.getElementById('Answer1').disabled = true;
     document.getElementById('Answer2').disabled = true;
@@ -96,6 +131,8 @@ function Answer3Select(){
     ScoreTotal.push(score[i][j])
     document.getElementById("Rolling Score").innerHTML="Your total score is"+'<br>'+ScoreTotal.reduce(getSum);
     }else{
+      
+      document.getElementById('Answer3').style.backgroundColor = 'greenyellow';
       document.getElementById("Finish").style.display = "inline";
     document.getElementById("CurrentQuScore").style.display = "block";
           document.getElementById("Answer1").disabled = true;
@@ -106,39 +143,3 @@ function Answer3Select(){
     document.getElementById("Rolling Score").innerHTML="Your total score is"+'<br>'+ScoreTotal.reduce(getSum);
     }
 }
-var Q1 = 'Which planet is known as the red planet';
-var Q2 = 'What is the closest planet to the sun?';
-var Q3 = 'Where can sun be classified?';
-var Q4 = 'Which galaxy is earth located?';
-var Q5 = ' What is the name of NASA’s most famous space telescope?';
-var Questions = [Q1, Q2, Q3, Q4, Q5];
-
-var A1 = ['Jupiter', 'Mars', 'Saturn']
-var A2 = ['Uranus', 'Saturn', 'Mercury'];
-var A3 = ['Planet', 'Star', 'No idea'];
-var A4 = ['Andromeda Galaxy', 'Whirlpool Galaxy', 'The Milky Way Galaxy'];
-var A5 = ['James Webb telescope', 'Spitzer space telescope', 'Hubble Space telescope']
-var Answers = [A1, A2, A3, A4, A5];
-
- var SN1 = 'Mars';
- var SN2 = 'Mercury';
- var SN3 = 'Planet';
- var SN4 = 'The Milky Way Galaxy';
- var SN5 = 'Jupiter';
- var Soln = [SN1, SN2, SN3, SN4, SN5];
-
-var SC1 = [0, 10, 0];
-var SC2 = [0, 0, 10];
-var SC3 = [0, 10, 0];
-var SC4 = [0, 0, 10];
-var SC5 = [0, 0, 10];
-var SC6 = [0, 0, 10]
-var score = [SC1, SC2, SC3, SC4, SC5, SC6];
-function getSum(a, b){
-  return a + b;
-}
-document.getElementById("QuestionQuiz").innerHTML = "Question 1:"+Questions[i];
-document.getElementById("QuestionQuiz").style.display = 'block';
-document.getElementById("Answer1").innerHTML=Answers[i][0];
-document.getElementById("Answer2").innerHTML=Answers[i][1];
-document.getElementById("Answer3").innerHTML=Answers[i][2];
